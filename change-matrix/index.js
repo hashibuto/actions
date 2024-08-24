491fc903d11bcfa19a780ab998fc542c625cda86
= <=>
2024-08-25 05:45:39 +0900

    updated

--- change-matrix/index.js
+++ change-matrix/index.js
@@ -1,11 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const path = require('node:path')
+const path = require('node:path');
 
 try {
   const baseDirectory = core.getInput('base-directory')
   const searchDirectory = path.normalize(path.join(__dirname, baseDirectory))
 
+  console.log(github.context)
   console.log(`searching directory ${searchDirectory}`)
 } catch (error) {
   core.setFailed(error.message);
