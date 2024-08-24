e0b3022d0886b1b2c9768a30a71332f0edc05e44
= <=>
2024-08-25 06:11:17 +0900

    updated

--- change-matrix/index.js
+++ change-matrix/index.js
@@ -3,28 +3,32 @@ const github = require('@actions/github');
 const glob = require('@actions/glob');
 const path = require('node:path');
 
-try {
-  const baseDirectory = core.getInput('base-directory')
-  const workingDir = process.env.GITHUB_WORKSPACE
-  const searchDirectory = path.normalize(path.join(workingDir, baseDirectory))
+async function action() {
+  try {
+    const baseDirectory = core.getInput('base-directory')
+    const workingDir = process.env.GITHUB_WORKSPACE
+    const searchDirectory = path.normalize(path.join(workingDir, baseDirectory))
 
-  console.log(`searching directory ${searchDirectory}`)
-  const includePatterns = core.getInput("include")
-  console.log(`using include patterns: ${includePatterns}`)
+    console.log(`searching directory ${searchDirectory}`)
+    const includePatterns = core.getInput("include")
+    console.log(`using include patterns: ${includePatterns}`)
 
-  const patterns = includePatterns.split(",")
-  const fqPatterns = []
-  for (let p in patterns) {
-    fqPatterns = [...fqPatterns, path.join(searchDirectory, p)]
+    const patterns = includePatterns.split(",")
+    const fqPatterns = []
+    for (let p in patterns) {
+      fqPatterns = [...fqPatterns, path.join(searchDirectory, p)]
+    }
+    const globber = await glob.create(fqPatterns.join('\n'))
+    const files = await globber.glob()
+
+    for (let f in files) {
+      console.log(f)
+    }
+
+
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  const globber = await glob.create(fqPatterns.join('\n'))
-  const files = await globber.glob()
-
-  for (let f in files) {
-    console.log(f)
-  }
-
-
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+action()
