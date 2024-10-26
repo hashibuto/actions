f450cdac0f3c2213a01d60991eb5e2095c38b54f
= <=>
2024-10-27 04:38:05 +0900

    added tagger

---
+++ tagger/index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function action() {
+  try {
+    const mainBranch = core.getInput('main-branch')
+    const githubToken = core.getInput('github-token')
+    const octokit = github.getOctokit(githubToken)
+
+    try {
+      await octokit.rest.checks.listForRef({
+        owner:  github.context.payload.repository.owner.login,
+        repo: github.context.payload.repository.name,
+        ref: `refs/heads/${versionTag}`,
+      })
+    } catch (error) {
+      throw new Error(`unable to locate branch named "${mainBranch}"`)
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+action();
